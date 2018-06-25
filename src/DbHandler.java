73543f80ba0391de017758803825fc6e693aef31
shashank <sssinghsyr@gmail.com>
2018-06-25 11:59:45 -0700

    removed warnings

--- src/DbHandler.java
+++ src/DbHandler.java
@@ -36,25 +36,4 @@ public class DbHandler {
 		return fut.thenApply(s -> {respHandlr.sendResponseToClient(fut);;
 									return s;});
 	}
-
-	private static String escapeNonAscii(String str) {
-		StringBuilder retStr = new StringBuilder();
-		for(int i=0; i<str.length(); i++) {
-			int cp = Character.codePointAt(str, i);
-			int charCount = Character.charCount(cp);
-			if (charCount > 1) {
-				i += charCount - 1; // 2.
-				if (i >= str.length()) {
-					throw new IllegalArgumentException("truncated unexpectedly");
-				}
-			}
-
-			if (cp < 128) {
-				retStr.appendCodePoint(cp);
-			} else {
-				retStr.append(String.format("\\u%x", cp));
-			}
-		}
-		return retStr.toString();
-	}
 }
