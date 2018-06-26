1ee7206436704179e8e3cb411ac150436402225e
shashank <sssinghsyr@gmail.com>
2018-06-26 13:59:40 -0700

    Closing connection after each submission fire

--- src/DbHandler.java
+++ src/DbHandler.java
@@ -7,19 +7,19 @@ import org.postgresql.sql2.PGConnection;
 
 public class DbHandler {
 	private DataSource ds;
-	private static List<PGConnection> connections = new LinkedList<PGConnection>();
-	private static final int MAX_CNT = 1;
+	//private static List<PGConnection> connections = new LinkedList<PGConnection>();
+	//private static final int MAX_CNT = 100;
 
 	DbHandler(String ipAddr){
 		ds = ConnectUtil.openDB(ipAddr);
-		for(int i=0; i<MAX_CNT; i++)
-			connections.add((PGConnection) ds.getConnection());
+		/*for(int i=0; i<MAX_CNT; i++)
+			connections.add((PGConnection) ds.getConnection());*/
 		//conn.connectDb();
 	}
 
 	public CompletableFuture<Integer> singleRowOperation(String sql){
 		System.out.println("Query: ["+sql+"]");
-		PGConnection conn = DbConnection.getConnection();
+		PGConnection conn = (PGConnection) ds.getConnection();
 		CompletableFuture<Integer> fut = conn.<Integer>rowOperation("SELECT i as t FROM numbers1")
 				.collect(CollectorUtils.singleCollector(Integer.class))
 				.submit()
@@ -29,23 +29,25 @@ public class DbHandler {
 	}
 
 	public CompletableFuture<Object> countOperation(String sql) {
-		PGConnection conn = DbConnection.getConnection();
+//		PGConnection conn = DbConnection.getConnection();
+		PGConnection conn = (PGConnection) ds.getConnection();
 		CompletableFuture<Object> fut = conn.countOperation(sql).submit().getCompletionStage().toCompletableFuture();
 		fut.exceptionally(ex -> {ClientQueryReceiver.selector.wakeup(); ex.printStackTrace(); return 0;});
 		return fut.thenApply(s -> {ClientQueryReceiver.selector.wakeup(); System.out.println("Print inside thenApply");return s;}); 
 	}
 
 	public CompletableFuture<String> multipleRowOperation(String sql, ResponseHandler respHandlr) {
-		PGConnection conn = DbConnection.getConnection();
+//		PGConnection conn = DbConnection.getConnection();
+		PGConnection conn = (PGConnection) ds.getConnection();
 		CompletableFuture<String> fut = conn.<String>rowOperation(sql)
 				.collect(CollectorUtils.rowCollector("csv"))
 				.submit()
 				.getCompletionStage().toCompletableFuture();
-		return fut.thenApply(s -> {respHandlr.sendResponseToClient(fut);;
+		return fut.thenApply(s -> {respHandlr.sendResponseToClient(fut);conn.close();
 									return s;});
 	}
 	
-	static class DbConnection{
+/*	static class DbConnection{
 		private static int state = 0;
 		public static PGConnection getConnection() {
 			state++;
@@ -53,5 +55,5 @@ public class DbHandler {
 				state = 0;
 			return connections.get(state);
 		}
-	}
+	}*/
 }
